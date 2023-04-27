3996d240b843eff4514fd00463ec446dd0ef4572
Shubrotto Kumar Hawlader <skshubroto@gmail.com>
2023-04-27 20:50:20 +0600

    jacob site

---
+++ index.js
@@ -0,0 +1,35 @@
+// toggle icon navbar
+
+const menuIcon = document.querySelector("#menu-icon");
+const navBar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navBar.classList.toggle("active");
+};
+
+// scroll section
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("header nav a");
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.screenY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top > offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + " id " + "]")
+          .classList.add("active");
+      });
+    }
+  });
+  let header = document.querySelector(".header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  menuIcon.classList.remove("bx-x");
+  navBar.classList.remove("active");
+};
